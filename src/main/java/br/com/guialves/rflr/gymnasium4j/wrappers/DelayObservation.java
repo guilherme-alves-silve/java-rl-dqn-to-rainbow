7bd986e0a79781a520f337c9e34dd2c7d90bb8bd
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2026-02-17 00:48:18 -0300

    :books: doc: Add documentation for the gymnasium wrappers

--- src/main/java/br/com/guialves/rflr/gymnasium4j/wrappers/DelayObservation.java
+++ src/main/java/br/com/guialves/rflr/gymnasium4j/wrappers/DelayObservation.java
@@ -1,5 +1,11 @@
 package br.com.guialves.rflr.gymnasium4j.wrappers;
 
+/**
+ * Equivalent to the Gymnasium (from Python):
+ *  <a href="https://gymnasium.farama.org/api/wrappers/observation_wrappers/#gymnasium.wrappers.DelayObservation">...</a>
+ *
+ * @param delay
+ */
 public record DelayObservation(int delay) implements IWrapper {
 
     @Override
