2a567bab23fe19d634f8cf3f252a1c5f1f1f65af
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2026-02-26 01:16:15 -0300

    :white_check_mark: tests: Increase code coverage

--- src/test/java/br/com/guialves/rflr/python/numpy/NumPyBufferViewTest.java
+++ src/test/java/br/com/guialves/rflr/python/numpy/NumPyBufferViewTest.java
@@ -3,6 +3,7 @@ package br.com.guialves.rflr.python.numpy;
 import org.junit.jupiter.api.*;
 
 import static br.com.guialves.rflr.python.PythonRuntime.*;
+import static br.com.guialves.rflr.python.numpy.NumPyByteBuffer.toDoubleArray;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -45,11 +46,30 @@ class NumPyBufferViewTest {
             assertEquals(1.0f, buffer.get(0), 0.0001f);
             assertEquals(2.0f, buffer.get(1), 0.0001f);
             assertEquals(3.0f, buffer.get(2), 0.0001f);
+            buffer.put(0, 4f);
+            buffer.put(1, 5f);
+            buffer.put(2, 6f);
+            assertArrayEquals(new double[]{4., 5., 6.}, toDoubleArray(pyArr), 0.0001);
         }
     }
 
     @Test
     @Order(4)
+    void shouldWhenViewDeallocateTheArrShouldExists() {
+        exec("import numpy as np; arr = np.array([1.0, 2.0, 3.0], dtype=np.float32)");
+        var pyArr = eval("arr");
+        try (var view = new NumPyBufferView(pyArr)) {
+            var buffer = view.asFloatBuffer();
+            assertEquals(1.0f, buffer.get(0), 0.0001f);
+            assertEquals(2.0f, buffer.get(1), 0.0001f);
+            assertEquals(3.0f, buffer.get(2), 0.0001f);
+        }
+        assertArrayEquals(new double[]{1., 2., 3.}, toDoubleArray(pyArr), 0.0001);
+        pyArr.close();
+    }
+
+    @Test
+    @Order(5)
     void shouldThrowWhenInvalidPyObject() {
         try (var pyLong = eval("42")) {
             assertThrows(IllegalStateException.class, () -> new NumPyBufferView(pyLong));
