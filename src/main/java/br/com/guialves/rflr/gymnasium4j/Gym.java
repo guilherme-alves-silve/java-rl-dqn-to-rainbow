ef181bd3db8f7df906f70012dac2c3651830a2fe
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2026-03-02 00:49:24 -0300

    :art: refactor: Make some adjustments to make the classes more readable

--- src/main/java/br/com/guialves/rflr/gymnasium4j/Gym.java
+++ src/main/java/br/com/guialves/rflr/gymnasium4j/Gym.java
@@ -7,16 +7,15 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
-import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.joining;
 
 @Slf4j
 public class Gym {
 
     @SneakyThrows
-    public static Env make(String name,
-                           NDManager ndManager) {
+    public static IEnv make(String name,
+                            NDManager ndManager) {
         return builder()
                 .envName(name)
                 .ndManager(ndManager)
@@ -112,13 +111,13 @@ public class Gym {
 
         private String generateImportLibsPy() {
             return importLibs.stream()
-                    .collect(Collectors.joining(", ", "import ", ""));
+                    .collect(joining(", ", "import ", ""));
         }
 
         private String generateImportFromPy() {
             return wrappers.stream()
                     .map(wrapper -> wrapper.getClass().getSimpleName())
-                    .collect(Collectors.joining(", ", "from gymnasium.wrappers import ", ""));
+                    .collect(joining(", ", "from gymnasium.wrappers import ", ""));
         }
 
         private String generateWrappedEnvPy() {
@@ -155,7 +154,8 @@ public class Gym {
          * @param value parameter value (will be used as-is in Python)
          * @return this builder for chaining
          */
-        public PyMap put(String key, String value) {
+        public PyMap put(@NonNull String key,
+                         @NonNull String value) {
             params.put(key, value);
             return this;
         }
@@ -166,7 +166,8 @@ public class Gym {
          * @param value boolean value (converted to Python True/False)
          * @return this builder for chaining
          */
-        public PyMap put(String key, boolean value) {
+        public PyMap put(@NonNull String key,
+                         boolean value) {
             params.put(key, value ? "True" : "False");
             return this;
         }
@@ -177,7 +178,8 @@ public class Gym {
          * @param value integer value
          * @return this builder for chaining
          */
-        public PyMap put(String key, int value) {
+        public PyMap put(@NonNull String key,
+                         int value) {
             params.put(key, String.valueOf(value));
             return this;
         }
@@ -188,7 +190,8 @@ public class Gym {
          * @param value double value
          * @return this builder for chaining
          */
-        public PyMap put(String key, double value) {
+        public PyMap put(@NonNull String key,
+                         double value) {
             params.put(key, String.valueOf(value));
             return this;
         }
@@ -199,7 +202,8 @@ public class Gym {
          * @param value string value
          * @return this builder for chaining
          */
-        public PyMap putStr(String key, String value) {
+        public PyMap putStr(@NonNull String key,
+                            @NonNull String value) {
             params.put(key, "'" + value + "'");
             return this;
         }
@@ -229,7 +233,7 @@ public class Gym {
             return params.entrySet()
                     .stream()
                     .map(entry -> entry.getKey() + "=" + entry.getValue())
-                    .collect(Collectors.joining(", "));
+                    .collect(joining(", "));
         }
 
         /**
@@ -241,7 +245,7 @@ public class Gym {
             return params.entrySet()
                     .stream()
                     .map(entry -> "'" + entry.getKey() + "': " + entry.getValue())
-                    .collect(Collectors.joining(", ", "{", "}"));
+                    .collect(joining(", ", "{", "}"));
         }
 
         @Override
--- src/main/java/br/com/guialves/rflr/gymnasium4j/IEnv.java
+++ src/main/java/br/com/guialves/rflr/gymnasium4j/IEnv.java
@@ -1,6 +1,5 @@
 package br.com.guialves.rflr.gymnasium4j;
 
-import ai.djl.Device;
 import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDManager;
 import ai.djl.util.Pair;
@@ -24,11 +23,14 @@ public interface IEnv extends AutoCloseable {
 
     Pair<Map<Object, Object>, NDArray> reset();
 
-    EnvStepResult step(ActionSpaceType.ActionResult action) ;
+    EnvStepResult step(ActionSpaceType.ActionResult action);
 
     EnvStepResult step(ActionSpaceType.ActionResult action, NDManager manager);
 
     BufferedImage render();
 
     NDManager manager();
+
+    @Override
+    void close();
 }
